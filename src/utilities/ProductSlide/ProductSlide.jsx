f511f5b2bb0ba9d11fd1c2c39f18d2c1914e6640
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-07-30 17:56:09 +0300

    Create ProductSlide component.

---
+++ src/utilities/ProductSlide/ProductSlide.jsx
@@ -0,0 +1,28 @@
+// eslint-disable-next-line no-unused-vars
+import { Link } from 'react-router-dom'
+import styles from './ProductSlide.module.css'
+import PropTypes from 'prop-types'
+
+function ProductSlide({ img, title, price, link }) {
+
+    return (
+        <Link to={link}>
+            <div className={styles.slideOne}>
+                <div className={styles.productOne}>
+                    <img className={styles.sliderProdImage} src={img} width='70%' alt={title} />
+                    <div className={styles.sliderProdTitle}>{title}</div>
+                    <div className={styles.sliderProdPrice}>{price}</div>
+                </div>
+            </div>
+        </Link> 
+    )
+}
+
+ProductSlide.propTypes = {
+    img: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired
+}
+
+export default ProductSlide
