d044f1a1da5e125dea0700ec6af73ab4d4452e8c
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-08-01 10:53:00 +0300

    Fetch data from API on load for each product category.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 
 
@@ -6,8 +6,43 @@ import { Outlet } from "react-router-dom";
 import styles from "./App.module.css";
 
 import Header from "../Header/Header";
+import { func } from "prop-types";
 
 function App() {
+
+  const [loading, setLoading] = useState(true)
+  const [carouselProducts, setCarouselProducts] = useState({})
+
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/category/women's clothing`, {mode: "cors"})
+    .then((data) => data.json())
+    .then((data) => setCarouselProducts((prevProducts) => ({
+      ...prevProducts,
+      "women's clothing": data
+    })))
+    fetch(`https://fakestoreapi.com/products/category/men's clothing`, {mode: "cors"})
+    .then((data) => data.json())
+    .then((data) => setCarouselProducts((prevProducts) => ({
+      ...prevProducts,
+      "men's clothing": data
+    })))
+    fetch(`https://fakestoreapi.com/products/category/jewelery`, {mode: "cors"})
+    .then((data) => data.json())
+    .then((data) => setCarouselProducts((prevProducts) => ({
+      ...prevProducts,
+      "jewelery": data
+    })))
+    fetch(`https://fakestoreapi.com/products/category/electronics`, {mode: "cors"})
+    .then((data) => data.json())
+    .then((data) => setCarouselProducts((prevProducts) => ({
+      ...prevProducts,
+      "electronics": data
+    })))
+    .finally(setLoading(false))
+  }, [])
+
+  if (loading) return <p>Loading...</p>
+
   return (
     <>
       <Header></Header>
