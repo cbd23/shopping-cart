8c40dec1097b8d7d56db69d1e03d54e831869abc
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-08-05 23:37:33 +0300

    Add logic needed to display womenswear products.

--- src/components/Womenswear/Womenswear.jsx
+++ src/components/Womenswear/Womenswear.jsx
@@ -1,8 +1,28 @@
 import styles from './Womenswear.module.css'
+import ShopInventory from '../../utilities/shop-stock.json'
+import { Link } from 'react-router-dom'
 
 function Womenswear() {
+    
+    const womenswearProducts = ShopInventory.womenswear.map((item) => (
+        <Link to={'/shop/womenswear/' + item.id} key={item.id} className={styles.productContainer}>
+            <div className={styles.imageContainer}>
+                <img className={styles.imageOne} src={item.imageOne} alt={item.title} height='220px' />
+            </div>
+            <div className={styles.textContainer}>
+                <div className={styles.itemTitle}>{item.title}</div>
+                <span className={styles.itemPrice}>{item.price + ' eur'}</span>
+            </div>
+        </Link>  
+    ))
+
+    const isMounted = true
+    if (!isMounted) return <h1 className={styles.loading}>Loading...</h1>
+
     return (
-        <h1>Womenswear placeholder</h1>
+        <main className={styles.main}>
+            {womenswearProducts}
+        </main>
     )
 }
 
