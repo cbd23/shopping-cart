80e7d9c7e296c4815c89a1a14689f6742472ee1b
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-08-05 23:40:44 +0300

    Add logic needed to display kidswear products.

--- src/components/Kidswear/Kidswear.jsx
+++ src/components/Kidswear/Kidswear.jsx
@@ -1,8 +1,28 @@
 import styles from './Kidswear.module.css'
+import ShopInventory from '../../utilities/shop-stock.json'
+import { Link } from 'react-router-dom'
 
 function Kidswear() {
+    
+    const kidswearProducts = ShopInventory.kidswear.map((item) => (
+        <Link to={'/shop/kidswear/' + item.id} key={item.id} className={styles.productContainer}>
+            <div className={styles.imageContainer}>
+                <img className={styles.imageOne} src={item.imageOne} alt={item.title} height='220px' />
+            </div>
+            <div className={styles.textContainer}>
+                <div className={styles.itemTitle}>{item.title}</div>
+                <span className={styles.itemPrice}>{item.price + ' eur'}</span>
+            </div>
+        </Link>  
+    ))
+
+    const isMounted = true
+    if (!isMounted) return <h1 className={styles.loading}>Loading...</h1>
+
     return (
-        <h1>Kidswear placeholder</h1>
+        <main className={styles.main}>
+            {kidswearProducts}
+        </main>
     )
 }
 
