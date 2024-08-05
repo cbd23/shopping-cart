b7aeea479a2b1f03535ec68b2303b192327bad81
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-08-05 16:32:48 +0300

    Remove placeholders & display the desired products.

--- src/utilities/AutoPlayMethods/AutoPlayMethods.jsx
+++ src/utilities/AutoPlayMethods/AutoPlayMethods.jsx
@@ -6,8 +6,10 @@ import "slick-carousel/slick/slick-theme.css";
 import styles from './AutoPlayMethods.module.css'
 
 import ProductSlide from "../ProductSlide/ProductSlide";
+import ShopInventory from '../shop-stock.json'
+import Shop from "../../components/Shop/Shop";
 
-function AutoPlayMethods() {
+function AutoPlayMethods({ carouselItems }) {
   let sliderRef = useRef(null);
 
   const settings = {
@@ -18,15 +20,19 @@ function AutoPlayMethods() {
     autoplay: true,
     autoplaySpeed: 2500
   };
+
+
+  console.log(ShopInventory)
+
   return (
     <div className={styles.sliderContainer}>
       <Slider className={styles.mainSlider} ref={slider => (sliderRef = slider)} {...settings}>
-        <ProductSlide img={"https://static.zara.net/assets/public/a645/fa72/7f1e426f8b12/c3eb80f2e677/01165787250-e1/01165787250-e1.jpg?ts=1712737330923&w=824"} title={'Black Hearted T-Shirt'} price={'$29.99'} link={'/shop'} />
-        <ProductSlide img={"https://static.zara.net/assets/public/8452/3b11/43ff478fb1aa/70cde93bcad0/02224664807-e1/02224664807-e1.jpg?ts=1710245015382&w=824"} title={'Black Hearted T-Shirt'} price={'$29.99'} link={'/shop'} />
-        <ProductSlide img={"https://static.zara.net/assets/public/8dab/9d0a/8fc740f09501/fc5d6cd95f83/06224419800-e2/06224419800-e2.jpg?ts=1706026602704"} title={'Black Hearted T-Shirt'} price={'$29.99'} link={'/shop'} />
-        <ProductSlide img={"https://static.zara.net/assets/public/38a0/8f65/24c0491f94bb/19a033c4d973/01716381640-e1/01716381640-e1.jpg?ts=1710949543829"} title={'Black Hearted T-Shirt'} price={'$29.99'} link={'/shop'} />
-        <ProductSlide img={"https://static.zara.net/assets/public/0d59/bbe8/f37646f290e2/0ce10cc56438/07878534806-e1/07878534806-e1.jpg?ts=1708589938887&w=824"} title={'Black Hearted T-Shirt'} price={'$29.99'} link={'/shop'} />
-        <ProductSlide img={"https://static.zara.net/assets/public/aa04/249a/248a4d7fb4ec/4817ae580cf3/06668538707-e1/06668538707-e1.jpg?ts=1707468759807&w=824"} title={'Black Hearted T-Shirt'} price={'$29.99'} link={'/shop'} />
+        <ProductSlide img={"https://raw.githubusercontent.com/cbd23/shopping-cart/main/src/images/shop-stock/menswear/men-t-shirt.jpg"} title={ShopInventory.menswear[0].title} price={ShopInventory.menswear[0].price} link={'/shop'} />
+        <ProductSlide img={"https://raw.githubusercontent.com/cbd23/shopping-cart/main/src/images/shop-stock/womenswear/women-dress.jpg"} title={ShopInventory.womenswear[0].title} price={ShopInventory.womenswear[0].price} link={'/shop'} />
+        <ProductSlide img={"https://raw.githubusercontent.com/cbd23/shopping-cart/main/src/images/shop-stock/menswear/men-shoes.jpg"} title={ShopInventory.menswear[2].title} price={ShopInventory.menswear[2].price} link={'/shop'} />
+        <ProductSlide img={"https://raw.githubusercontent.com/cbd23/shopping-cart/main/src/images/shop-stock/womenswear/women-shirt.jpg"} title={ShopInventory.womenswear[1].title} price={ShopInventory.womenswear[1].price} link={'/shop'} />
+        <ProductSlide img={"https://raw.githubusercontent.com/cbd23/shopping-cart/main/src/images/shop-stock/menswear/men-pants.jpg"} title={ShopInventory.menswear[1].title} price={ShopInventory.menswear[1].price} link={'/shop'} />
+        <ProductSlide img={"https://raw.githubusercontent.com/cbd23/shopping-cart/main/src/images/shop-stock/kidswear/kids-sunglasses.jpg"} title={ShopInventory.kidswear[4].title} price={ShopInventory.kidswear[4].price} link={'/shop'} />
       </Slider>
       <div style={{ textAlign: "center" }}>
       </div>
