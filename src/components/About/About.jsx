2834adac25f3477d907d0956f8b6cec597f7640c
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-08-05 16:23:20 +0300

    Add About component (with placeholders).

---
+++ src/components/About/About.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import styles from './About.module.css'
+
+function About() {
+    return (
+        <main className={styles.main}>
+          <h1 className={styles.shop}>ABOUT</h1>
+        </main>
+    )
+  }
+  
+  export default About
