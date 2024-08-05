93c57bd28f853938a997daa6ff8939e3a2b85dd5
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-08-05 16:27:08 +0300

    Update copy on Home.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import styles from "./Home.module.css";
-import { Link } from "react-router-dom";
+import { Link, useOutletContext } from "react-router-dom";
 
 import AutoPlayMethods from "../../utilities/AutoPlayMethods/AutoPlayMethods";
 
 function Home() {
+
   return (
       <main className={styles.main}>
         <h1 className={styles.heading}>Welcome to <span className={styles.logoOnText}>BADILA&apos;s</span>.</h1>
-        <p className={styles.para}>Discover our latest products, whether you&apos;re looking for clothes, jewelry or electronics.<br /><br />Free shipping for orders over $100.</p>
+        <p className={styles.para}>Dress to impress. Dress to express. Dress to whatever... or don&apos;t even dress.<br /><br />Free shipping for orders over 100 eur.</p>
         <Link to='/shop'>
           <button className={styles.shopNowBtn}>Shop Now</button>
         </Link>
