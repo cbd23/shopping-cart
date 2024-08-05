abcb506cd0632c9e77ba892ad16279d354532bb2
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-08-05 16:25:47 +0300

    Add Favorites component (with placeholders).

---
+++ src/components/Favorites/Favorites.jsx
@@ -0,0 +1,11 @@
+import styles from './Favorites.module.css'
+
+function Favorites() {
+    return (
+        <main className={styles.main}>
+          <h1 className={styles.cart}>Favorites</h1>
+        </main>
+    )
+}
+
+export default Favorites
