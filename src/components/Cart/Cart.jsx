5572a504bd14dd0a76b679de97816fe8e93908ff
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-08-05 16:25:31 +0300

    Add Cart component (with placeholders).

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,11 @@
+import styles from './Cart.module.css'
+
+function Cart() {
+    return (
+        <main className={styles.main}>
+          <h1 className={styles.cart}>CART</h1>
+        </main>
+    )
+}
+
+export default Cart
