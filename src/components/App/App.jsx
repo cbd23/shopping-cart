c590a8ed9a91faf997cfabfb1ce00ad5667aadab
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-08-16 20:35:31 +0300

    Create a fn to add/remove fav products & store them into an array.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 
 // eslint-disable-next-line no-unused-vars
@@ -5,14 +6,32 @@ import styles from "./App.module.css";
 import Header from "../Header/Header";
 
 function App() {
+
+  const [favoriteProducts, setFavoriteProducts] = useState([])
+
+  // useEffect(() => {
+  //   console.log(favoriteProducts)
+  // }, [favoriteProducts])
   
   const isMounted = true
   if (!isMounted) return <h1 className={styles.loading}>Loading...</h1>
 
+  
+  // add/remove products to/from Favorites, depending if they're added or not
+  function updateFavorites(id) {
+    if (favoriteProducts.includes(id)) {
+      console.log(`Product with id ${id} was removed from Favorites.`)
+      setFavoriteProducts(favoriteProducts.filter(product => product !== id))
+    } else {
+      console.log(`Product with id ${id} was added to Favorites.`)
+      setFavoriteProducts([...favoriteProducts, id])
+    }
+  }
+
   return (
     <>
       <Header></Header>
-      <Outlet />
+      <Outlet context={{favoriteProducts, updateFavorites}} />
     </>
   );
 }
